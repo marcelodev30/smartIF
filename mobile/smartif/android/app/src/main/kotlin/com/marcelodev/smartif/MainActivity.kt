96c70de2055a65aa4893cc849b44295b223a90a4
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-08-25 14:01:40 -0300

    add init project

---
+++ mobile/smartif/android/app/src/main/kotlin/com/marcelodev/smartif/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.marcelodev.smartif
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ mobile/smartif/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:smartif/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
