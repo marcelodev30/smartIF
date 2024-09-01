1024e9094ecada01540e3e4b7fe446ed034f936a
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2024-08-31 23:44:56 -0300

    add

--- server/arCondicionado/models.py
+++ server/arCondicionado/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    TIPO_USER=[
+        ('',''),
+        ('','')
+    ]
+    login = models.CharField(max_length=50)
+    senha = models.CharField(max_length=255)
+    nivel = models.CharField(max_length=30,choices=TIPO_USER,default=TIPO_USER[1]) 
+    img_url = models.CharField(max_length=50)
+    nome = models.CharField(max_length=100)
+   
+class Dispositivos(models.Model):
+    nome = models.CharField(max_length=255)
+    codigo_ir = models.CharField(max_length=20)
+    temperatura = models.CharField(max_length=6)
+    status = models.BooleanField()
+    #id_loca=models.ForeignKey()
+
+class ServerMQTT(models.Model):
+    usuario = models.CharField(max_length=50)  
+    senha = models.CharField(max_length=255)
+    host = models.CharField(max_length=13)
+
+class Loca(models.Model):
+    nome = models.CharField(max_length=255)
