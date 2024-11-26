e9fc78c0282286dbccfc4331949e6c2a066cb6de
Marcelo Alves <161260493+marcelodev30@users.noreply.github.com>
2024-11-25 21:00:40 -0300

    Delete mobile/smartif directory

--- mobile/smartif/android/app/src/main/kotlin/com/marcelodev/smartif/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.marcelodev.smartif
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- mobile/smartif/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:smartif/my_app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
